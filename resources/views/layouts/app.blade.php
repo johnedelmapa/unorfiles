cf00ddd03025686d06a6717b4c3148e8e34d6207
Johnedel <mapajohnedel@gmail.com>
2018-04-21 16:06:32 +0800

    Second Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title>{{config('app.name', 'UNOR Files')}}</title>
+        <link rel="shortcut icon" type="image/x-icon" href="{{asset('img/unor.png')}}" />
     </head>
     <body>
         @include('inc.navbar')
